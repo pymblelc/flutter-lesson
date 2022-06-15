9ab5f492f4bebc41a676f66fa4d6180dbb1f32c8
Mr Josh <mr-josh@sparklapse.com>
2022-06-15 16:20:58 +1000

    Updated template
    Replaced shared_preferences with hive
    Updated completed demo

--- lib/done.dart
+++ lib/done.dart
@@ -1,12 +1,12 @@
 import "package:flutter/material.dart";
-import 'package:shared_preferences/shared_preferences.dart';
+import "package:hive_flutter/hive_flutter.dart";
 
-// Global variables
-SharedPreferences? prefs;
+const boxName = "counter";
 
 // Run and start our app
 void main() async {
-  prefs = await SharedPreferences.getInstance();
+  await Hive.initFlutter();
+  await Hive.openBox(boxName);
   runApp(const MyApp());
 }
 
@@ -34,27 +34,27 @@ class MyCounter extends StatefulWidget {
 // This is our actual app!
 class _MyCounterState extends State<MyCounter> {
   // This variable stores the current count (always starting at 0)
+  Box storage = Hive.box(boxName);
   int counter = 0;
-
-  // This function increases our counter by 1
-  void incrementCounter() async {
-    setState(() {
-      counter++;
-    });
-    await prefs?.setInt("counter", counter);
-  }
-
-  void decreaseCounter() async {
-    setState(() {
-      counter--;
-    });
-    await prefs?.setInt("counter", counter);
-  }
 
   @override
   void initState() {
     super.initState();
-    counter = (prefs?.getInt("counter") ?? 0);
+    counter = storage.get("count", defaultValue: 0);
+  }
+
+  // This function increases our counter by `amount`
+  void Function() changeCounter(int? amount) {
+    return () {
+      setState(() {
+        if (amount != null) {
+          counter += amount;
+        } else {
+          counter = 0;
+        }
+      });
+      storage.put("count", counter);
+    };
   }
 
   // This function makes things appear on screen
@@ -75,14 +75,51 @@ class _MyCounterState extends State<MyCounter> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: decreaseCounter,
+                  onPressed: changeCounter(-5),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 71, 96, 129),
+                    ),
+                  ),
+                  child: Text("-5"),
+                ),
+                ElevatedButton(
+                  onPressed: changeCounter(-1),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 61, 90, 129),
+                    ),
+                  ),
                   child: Text("-1"),
                 ),
                 ElevatedButton(
-                  onPressed: incrementCounter,
+                  onPressed: changeCounter(1),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 60, 111, 179),
+                    ),
+                  ),
                   child: Text("+1"),
                 ),
+                ElevatedButton(
+                  onPressed: changeCounter(5),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 48, 135, 250),
+                    ),
+                  ),
+                  child: Text("+5"),
+                ),
               ],
+            ),
+            ElevatedButton(
+              onPressed: changeCounter(null),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Color.fromARGB(255, 204, 171, 171),
+                ),
+              ),
+              child: Text("Reset"),
             ),
           ],
         ),
